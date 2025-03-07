8cd2e842577a7a38f79fc222f1609442742dac82
Tanya Yenakieva <yastigma@gmail.com>
2025-03-07 19:18:00 +0200

    fix logo2

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,5 +2,15 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { Link } from "react-router";
 import CSS from "./Logo.module.css";
 export default function Logo() {
-    return (_jsx("div", { className: CSS.wrapper, children: _jsx(Link, { to: "/", children: _jsx("img", { src: "/public/logo.webp", alt: "logo image", className: CSS.logo }) }) }));
+  return _jsx("div", {
+    className: CSS.wrapper,
+    children: _jsx(Link, {
+      to: "/",
+      children: _jsx("img", {
+        src: "/logo.webp",
+        alt: "logo image",
+        className: CSS.logo,
+      }),
+    }),
+  });
 }
